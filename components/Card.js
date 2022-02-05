fa4e2f03c6b068658c56a1e18bf8f89cbd625a87
sakib00 <sakib.xc@gmail.com>
2022-02-05 13:29:45 +0600

    Created Card component
    -installed react-native-vector-icons
    -created card component

---
+++ components/Card.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+import Icon2 from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const Card = () => {
+  return (
+    <View style={styles.card}>
+      <Text style={styles.textHead}>Author Name</Text>
+      <Text style={styles.textQoute}>
+        "Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s,"
+      </Text>
+      <View style={styles.actionBar}>
+        <Icon name="heart" size={20} color="#e63946" />
+        <Icon2 name="emoticon-excited" size={25} color="#ffd166" />
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 8,
+    paddingVertical: 35,
+    paddingHorizontal: 25,
+    marginHorizontal: 8,
+    marginVertical: 10,
+    borderColor: '#457b9d',
+    width: '96%',
+  },
+
+  textHead: {
+    fontSize: 10,
+    color: '#457b9d',
+  },
+  textQoute: {
+    fontSize: 14,
+    color: '#1d3557',
+    padding: 5,
+    fontWeight: '400',
+    fontStyle: 'italic',
+  },
+  actionBar: {
+    paddingHorizontal: 10,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
+});
+export default Card;
