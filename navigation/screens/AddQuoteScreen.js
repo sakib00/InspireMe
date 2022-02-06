a582ca58fd78f28d7ad633ef5ce49337dd1dbd80
sakib00 <sakib.xc@gmail.com>
2022-02-06 16:21:19 +0600

    Update AddQuoteScreen.js

--- navigation/screens/AddQuoteScreen.js
+++ navigation/screens/AddQuoteScreen.js
@@ -1,12 +1,73 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  TextInput,
+  Text,
+  TouchableHighlight,
+} from 'react-native';
 
 const AddQuoteScreen = () => {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const pressHandler = () => {};
   return (
-    <View>
-      <Text>Add Quote</Text>
-    </View>
+    <SafeAreaView style={styles.background}>
+      <TextInput
+        style={styles.input}
+        onChangeText={setAuthor}
+        value={author}
+        placeholder="Author Name"
+      />
+
+      <TextInput
+        style={styles.input}
+        onChangeText={setQuote}
+        value={quote}
+        placeholder="Enter your Quote"
+      />
+
+      <TouchableHighlight underlayColor="#a8dadc" onPress={pressHandler}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Add New Quote</Text>
+        </View>
+      </TouchableHighlight>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: '#a8dadc',
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    height: '100%',
+  },
+  button: {
+    alignItems: 'center',
+    marginHorizontal: 10,
+    backgroundColor: '#457b9d',
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'transparent',
+  },
+  buttonText: {
+    fontSize: 16,
+    color: 'white',
+  },
+  input: {
+    backgroundColor: 'white',
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 15,
+    borderColor: '#AAA',
+    padding: 10,
+  },
+});
 
 export default AddQuoteScreen;
